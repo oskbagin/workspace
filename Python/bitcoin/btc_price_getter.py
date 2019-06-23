1305214f9deeecb588e789866bcbf57c711f6cd6
Oskar Baginski <oskbag@gmail.com>
2019-06-23 16:29:06 +0200

    Getting prices now and today and average

--- Python/bitcoin/btc_price_getter.py
+++ Python/bitcoin/btc_price_getter.py
@@ -2,12 +2,38 @@ import requests
 
 class BtcPriceGetter:
     def __init__(self, period=''):
+        self.urlNow   = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
+        self.urlToday = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=daily&format=json'
+        self.urlMonth = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=monthly&format=json'
+        self.urlAlltime = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&format=json'
         self.url = ''
         self.response = ''
         self.json = ''
     
-    def priceNow(self):
-        btc_api_url_now = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/'
-        self.response = requests.get(btc_api_url_now)
+    def btcGet(self):
+        self.response = requests.get(self.url)
         self.json = self.response.json()
+
+    def priceNow(self):
+        self.url = self.urlNow
+        self.btcGet()        
         return float(self.json[0]['price_usd'])
+    
+    def pricesToday(self):
+        self.url = self.urlToday
+        self.btcGet()
+        return self.json
+
+    def priceTodayAverage(self):
+        self.pricesToday()
+
+        average = 0
+        for row in self.json:
+            if average == 0:
+                average = row['average']
+            else:
+                average += row['average']
+                average /= 2
+
+        return average
+
--- Python/bitcoin/getBTCPrice.py
+++ Python/bitcoin/getBTCPrice.py
@@ -2,10 +2,8 @@ import requests
 import time
 from btc_price_getter import BtcPriceGetter
 
-btc_api_url_day = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=daily&format=json'
-btc_api_url_month = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=monthly&format=json'
-btc_api_url_alltime = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&format=json'
-
 tmp = BtcPriceGetter()
-
 print(tmp.priceNow())
+average = tmp.priceTodayAverage()
+print(average)
+
