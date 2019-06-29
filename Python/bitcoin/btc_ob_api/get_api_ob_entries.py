bc14fb72ccdd102be136628922207a5579610c58
Oskar Baginski <oskbag@gmail.com>
2019-06-29 21:58:40 +0200

    Interface for getting data from config file instead of retrieving it in multiple files.

---
+++ Python/bitcoin/btc_ob_api/get_api_ob_entries.py
@@ -0,0 +1,30 @@
+import sys
+
+API_OB_FILE_NOT_FOUND    = 1
+API_OB_TOKEN_NOT_IN_FILE = 'TOKEN NOT FOUND'
+
+class getApiObEntries:
+    def __init__(self):
+        # The API requires to have api.ob file in home directory(assuming the repo was cloned to the home as well)
+        self.file = '../../../../api.ob'
+        try:
+            with open(self.file) as file:
+                self.apiObEntries = file.readlines()
+        except FileNotFoundError:
+            print('Couldn\'t open api.ob file. Aborting...')
+            sys.exit(API_OB_FILE_NOT_FOUND)
+        
+    def getEntry(self, token):
+        token += ':='
+        self.entry = ''
+        
+        for line in self.apiObEntries:
+            if token in line:
+                self.entry = line[len(token) : -1]
+                break
+        
+        if self.entry == '':
+            print('Could not retrieve token {} from the api.ob file. Handling in next steps...'.format(token[:-2]))
+            return API_OB_TOKEN_NOT_IN_FILE
+        
+        return self.entry
--- Python/bitcoin/ifttt_notification/ifttt_notification.py
+++ Python/bitcoin/ifttt_notification/ifttt_notification.py
@@ -1,23 +1,23 @@
 import requests
 import sys
+sys.path.append('..')
+from btc_ob_api.get_api_ob_entries import getApiObEntries, API_OB_TOKEN_NOT_IN_FILE
 
 class iftttNotification:
     def __init__(self):
-        # TODO create class to get the tokens from api.ob file
-        iftttKey = ''
-        token = 'iftttKey:='
-        with open('../../../api.ob') as file:
-            fileContent = file.readlines()
-            for line in fileContent:
-                if token in line:
-                    iftttKey = line[len(token):-1]
-                    break
-        if iftttKey == '':
-            print('Could not retrieve IFTTT applet key.')
+        keyToken = 'iftttKey'
+
+        # retrieve the keyToken value from the config file
+        iftttKey = getApiObEntries().getEntry(keyToken)
+
+        if iftttKey == API_OB_TOKEN_NOT_IN_FILE:
+            print('Could not retrieve IFTTT applet key. Sending error notification and aborting...')
             # TODO Send failed init notification
-            sys.exit(1)
+            sys.exit(2)
         else:
             self.postUrl = 'https://maker.ifttt.com/trigger/btc-price/with/key/' + iftttKey
     
     def iftttBtcPriceNotify(self):
         requests.post(self.postUrl)
+
+tmp = iftttNotification()
